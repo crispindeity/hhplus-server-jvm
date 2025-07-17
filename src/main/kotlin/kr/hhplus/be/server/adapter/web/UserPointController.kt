19ea376f402a415cd5558ee8816ef3c1aaa912e1
crispin <h.c.shin.dev09@gmail.com>
2025-07-17 23:58:59 +0900

    :clown_face: Mock: 포인트 조회 api 컨트롤러 추가

--- src/main/kotlin/kr/hhplus/be/server/adapter/web/UserPointController.kt
+++ src/main/kotlin/kr/hhplus/be/server/adapter/web/UserPointController.kt
@@ -2,7 +2,9 @@ package kr.hhplus.be.server.adapter.web
 
 import kr.hhplus.be.server.adapter.web.dto.ApiResponse
 import kr.hhplus.be.server.adapter.web.dto.request.ChargePointsRequest
+import kr.hhplus.be.server.adapter.web.dto.response.FindUserPointResponse
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -19,6 +21,23 @@ internal class UserPointController {
     ): ResponseEntity<ApiResponse<Long>> {
         if (request.amount > 0) {
             return ResponseEntity.ok().body(ApiResponse.success(result = id))
+        }
+        return ResponseEntity
+            .badRequest()
+            .body(ApiResponse.fail(code = 400, message = "bad request"))
+    }
+
+    @GetMapping("/{id}/points")
+    fun findUserPoint(
+        @PathVariable id: Long
+    ): ResponseEntity<ApiResponse<FindUserPointResponse>> {
+        val response =
+            FindUserPointResponse(
+                userId = id,
+                balance = 1000L
+            )
+        if (id > 0) {
+            return ResponseEntity.ok().body(ApiResponse.success(result = response))
         }
         return ResponseEntity
             .badRequest()
---
+++ src/main/kotlin/kr/hhplus/be/server/adapter/web/dto/response/FindUserPointResponse.kt
@@ -0,0 +1,6 @@
+package kr.hhplus.be.server.adapter.web.dto.response
+
+internal data class FindUserPointResponse(
+    val userId: Long,
+    val balance: Long
+)
