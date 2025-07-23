c53e0988c3f8f355cb355132db2fdaae5e6b4600
crispin <h.c.shin.dev09@gmail.com>
2025-07-23 11:59:32 +0900

    :sparkles: Feat: 대기열 토큰 검증 인터셉터 추가

--- src/main/kotlin/kr/hhplus/be/server/common/exception/ErrorCode.kt
+++ src/main/kotlin/kr/hhplus/be/server/common/exception/ErrorCode.kt
@@ -11,5 +11,7 @@ enum class ErrorCode(
 ) : CodeInterface {
     FAILED_TO_INVOKE_IN_LOG(code = 100, message = "failed to invoke in log."),
     INVALID_REQUEST_VALUE(code = 400, message = "invalid request value."),
-    TOKEN_ALREADY_ISSUED(code = 400, message = "token already issued.")
+    TOKEN_ALREADY_ISSUED(code = 400, message = "token already issued."),
+    MISSING_ENTRY_QUEUE_JWT_HEADER(code = 400, message = "missing entry queue token header."),
+    INVALID_JWT(code = 403, message = "invalid jwt.")
 }
