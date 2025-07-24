476c5e1c3f5898cf89e25574b416cec037a68421
crispin <h.c.shin.dev09@gmail.com>
2025-07-25 00:25:50 +0900

    :sparkles: Feat: 콘서트 좌석 상태 변경 기능 추가

--- src/main/kotlin/kr/hhplus/be/server/domain/ConcertSeat.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/ConcertSeat.kt
@@ -1,5 +1,8 @@
 package kr.hhplus.be.server.domain
 
+import kr.hhplus.be.server.common.exception.CustomException
+import kr.hhplus.be.server.common.exception.ErrorCode
+
 internal data class ConcertSeat(
     val id: Long,
     val scheduleId: Long,
@@ -10,5 +13,14 @@ internal data class ConcertSeat(
         HELD,
         AVAILABLE,
         RESERVED
+    }
+
+    fun held(): ConcertSeat {
+        if (status != SeatStatus.AVAILABLE) {
+            throw CustomException(ErrorCode.ALREADY_RESERVED)
+        }
+        return this.copy(
+            status = SeatStatus.HELD
+        )
     }
 }
