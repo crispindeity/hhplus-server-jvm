8593149c76736c44451dfa9c4a1dc9e0f83028aa
crispin <h.c.shin.dev09@gmail.com>
2025-08-13 15:42:24 +0900

    :white_check_mark: Test: 락 방식 변경으로 실패하는 테스트 수정

--- src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.kt
+++ src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.kt
@@ -2,6 +2,7 @@ package kr.hhplus.be.server
 
 import jakarta.annotation.PreDestroy
 import org.springframework.context.annotation.Configuration
+import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.utility.DockerImageName
 
@@ -18,17 +19,26 @@ class TestcontainersConfiguration {
                 .withDatabaseName("hhplus")
                 .withUsername("test")
                 .withPassword("test")
-                .apply {
-                    start()
-                }
+                .apply { start() }
+
+        val redisContainer: GenericContainer<*> =
+            GenericContainer(DockerImageName.parse("redis:8.2"))
+                .withExposedPorts(6379)
+                .apply { start() }
 
         init {
             System.setProperty(
                 "spring.datasource.url",
-                mySqlContainer.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC"
+                mySqlContainer.jdbcUrl + "?characterEncoding=UTF-8&serverTimezone=UTC"
             )
             System.setProperty("spring.datasource.username", mySqlContainer.username)
             System.setProperty("spring.datasource.password", mySqlContainer.password)
+
+            System.setProperty("spring.data.redis.host", redisContainer.host)
+            System.setProperty(
+                "spring.data.redis.port",
+                redisContainer.getMappedPort(6379).toString()
+            )
         }
     }
 }
