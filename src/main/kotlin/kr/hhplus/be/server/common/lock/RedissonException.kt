dd42707735131380f76812b1dc45080955c7b3ca
crispin <h.c.shin.dev09@gmail.com>
2025-08-13 15:41:10 +0900

    :sparkles: Feat: redis lock 예외 추가

--- src/main/kotlin/kr/hhplus/be/server/common/lock/RedissonException.kt
+++ src/main/kotlin/kr/hhplus/be/server/common/lock/RedissonException.kt
@@ -3,7 +3,15 @@ package kr.hhplus.be.server.common.lock
 import kr.hhplus.be.server.common.exception.BaseException
 import kr.hhplus.be.server.common.exception.ErrorCode
 
-class RedissonException(
+open class RedissonException(
     val code: ErrorCode,
     message: String? = null
 ) : BaseException(codeInterface = code, additionalMessage = message)
+
+class LockAcquisition(
+    message: String? = null
+) : RedissonException(ErrorCode.LOCK_BUSY, message)
+
+class RedisLockTimeoutException(
+    message: String? = null
+) : RedissonException(ErrorCode.LOCK_ACQUIRE_TIME_OUT, message)
