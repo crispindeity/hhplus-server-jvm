a8b082867cab54abe0e43cd76bf3244a90238080
crispin <h.c.shin.dev09@gmail.com>
2025-08-29 00:47:20 +0900

    :sparkles: Feat: 좌석 점유 이벤트 기능 구현

--- src/main/kotlin/kr/hhplus/be/server/seat/application/event/SeatEventPublisher.kt
+++ src/main/kotlin/kr/hhplus/be/server/seat/application/event/SeatEventPublisher.kt
@@ -1,56 +1,52 @@
-package kr.hhplus.be.server.concertseat.application.event
+package kr.hhplus.be.server.seat.application.event
 
-import kr.hhplus.be.server.common.application.extensions.orThrow
-import kr.hhplus.be.server.common.exception.ErrorCode
 import kr.hhplus.be.server.common.log.Log
 import kr.hhplus.be.server.common.transactional.AfterCommitExecutor
 import kr.hhplus.be.server.common.transactional.Transactional
-import kr.hhplus.be.server.concertseat.application.port.ConcertSeatPort
-import kr.hhplus.be.server.concertseat.domain.ConcertSeat
-import kr.hhplus.be.server.concertseat.exception.ConcertSeatException
 import kr.hhplus.be.server.reservation.application.event.MakeReservationEvent
+import kr.hhplus.be.server.seathold.application.port.SeatHoldPort
+import kr.hhplus.be.server.seathold.domain.SeatHold
+import org.slf4j.Logger
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
-internal class ConcertSeatEventPublisher(
-    private val concertSeatPort: ConcertSeatPort,
-    private val transactional: Transactional,
+internal class SeatEventPublisher(
+    private val seatHoldPort: SeatHoldPort,
     private val afterCommitExecutor: AfterCommitExecutor,
-    private val eventPublisher: ApplicationEventPublisher
+    private val eventPublisher: ApplicationEventPublisher,
+    private val transactional: Transactional
 ) {
-    private val logger = Log.getLogger(this.javaClass)
+    private val logger: Logger = Log.getLogger(this.javaClass)
 
     @Async
     @EventListener
     fun handleMakeReservationEvent(event: MakeReservationEvent) {
         runCatching {
             Log.logging(logger) { log ->
-                log["method"] = "concertSeat.handleMakeReservationEvent()"
+                log["method"] = "handleMakeReservationEvent()"
                 log["eventId"] = event.eventId
                 transactional.run {
-                    val concertSeat: ConcertSeat =
-                        concertSeatPort
-                            .getConcertSeat(event.concertSeatId)
-                            .orThrow { ConcertSeatException(ErrorCode.NOT_FOUND_CONCERT_SEAT) }
-
-                    val heldSeat: ConcertSeat = concertSeat.held()
-                    concertSeatPort.update(heldSeat)
-
+                    seatHoldPort.save(
+                        SeatHold(
+                            concertSeatId = event.concertSeatId,
+                            userId = event.userId
+                        )
+                    )
                     afterCommitExecutor.registerAfterCommit {
                         eventPublisher.publishEvent(
-                            ConcertSeatHoldCompletedEvent(
+                            SeatHoldCompletedEvent(
                                 eventId = event.eventId,
                                 reservationId = event.reservationId,
-                                concertSeatId = event.concertSeatId
+                                seatId = event.seatId
                             )
                         )
                     }
 
                     afterCommitExecutor.registerAfterRollback {
-                        sendToConcertSeatHoldFailedEvent(event)
+                        sendToSeatHoldFailedEvent(event)
                     }
                 }
             }
@@ -59,12 +55,12 @@ internal class ConcertSeatEventPublisher(
         }
     }
 
-    private fun sendToConcertSeatHoldFailedEvent(event: MakeReservationEvent) {
+    private fun sendToSeatHoldFailedEvent(event: MakeReservationEvent) {
         eventPublisher.publishEvent(
-            ConcertSeatHoldFailedEvent(
+            SeatHoldFailedEvent(
                 eventId = event.eventId,
                 reservationId = event.reservationId,
-                concertSeatId = event.concertSeatId
+                seatId = event.seatId
             )
         )
     }
